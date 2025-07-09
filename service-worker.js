fa33bd8a5b98b8db76e6a08334f0d79fcbcefc76
Kuubicek <149311440+Kuubicek@users.noreply.github.com>
2025-07-10 00:06:49 +0200

    Update service-worker.js v2.1

--- service-worker.js
+++ service-worker.js
@@ -13,7 +13,22 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache =>
+      cache.addAll([
+        'index.html',
+        'kelimkar.html',
+        'stankar.html',
+        'nadrizeny.html',
+        'style.css',               // tady už bude čerstvé
+        'manifest.json',
+        '/android-launchericon-192-192.png',
+        '/android-launchericon-512-512.png',
+        'https://cdn.jsdelivr.net/npm/@supabase/supabase-js'
+      ])
+    )
+  );
 });
 
 self.addEventListener('fetch', e => {
@@ -21,8 +36,15 @@ self.addEventListener('fetch', e => {
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+  self.clients.claim();
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME)
+            .map(k => caches.delete(k))
+      )
+    )
+  );
   ));
 });
 
